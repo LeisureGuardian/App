4ffa0c1fcd54f19ad7f7c0ecd8e72bca6df888eb
SeongMinK <eo1tks3@naver.com>
2021-06-13 22:44:54 +0900

    fragment -> FragmentContainerView

--- app/src/main/java/kr/ac/kumoh/ce/leisureguardian/TabActivity.kt
+++ app/src/main/java/kr/ac/kumoh/ce/leisureguardian/TabActivity.kt
@@ -3,7 +3,7 @@ package kr.ac.kumoh.ce.leisureguardian
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
@@ -15,8 +15,8 @@ class TabActivity : AppCompatActivity() {
         setContentView(R.layout.activity_tab)
 
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
-
-        val navController = findNavController(R.id.nav_host_fragment)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(setOf(
