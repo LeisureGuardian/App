3247cd6022ae5a06a2c4d6be5f15f26faf5bd2d7
Kimsuaje <123kswja@gmail.com>
2021-06-16 02:37:55 +0900

    device add and delete

--- app/src/main/java/kr/ac/kumoh/s20160250/lg/ui/home/HomeViewModel.kt
+++ app/src/main/java/kr/ac/kumoh/s20160250/lg/ui/home/HomeViewModel.kt
@@ -10,6 +10,7 @@ import kr.ac.kumoh.s20160250.lg.MySingleton
 import kr.ac.kumoh.s20160250.lg.data.LoginData
 import kr.ac.kumoh.s20160250.lg.data.ResponseData
 import kr.ac.kumoh.s20160250.lg.data.RetrofitAPI
+import kr.ac.kumoh.s20160250.lg.data.StatusData
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -18,10 +19,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class HomeViewModel (application: Application) : AndroidViewModel(application) {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = MySingleton.getInstance(application).login_token
-    }
-    val text: LiveData<String> = _text
+//    val list: MutableLiveData<ArrayList<StatusData>>()
 
 
 
